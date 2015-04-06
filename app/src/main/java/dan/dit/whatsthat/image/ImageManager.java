a5e4d12c84143a062a0e844a4ab934c8b33f957d
Crackle42 <fab.dittmar@web.de>
2015-04-06 17:25:11 +0200

    Added pics.
    About 40

--- app/src/main/java/dan/dit/whatsthat/image/ImageManager.java
+++ app/src/main/java/dan/dit/whatsthat/image/ImageManager.java
@@ -1,4 +1,4 @@
-package dan.dit.whatsthat.image;
+﻿package dan.dit.whatsthat.image;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -198,6 +198,39 @@ public class ImageManager {
         author = new ImageAuthor("stux", "pixabay.com/en/castle-padlock-shut-off-to-378353", "CC0 Public Domain", "castle", null);
         easyBuild(context, author, newImages,
                 R.drawable.castle, "SCHLOSS", "LOCK");
+
+
+author = new ImageAuthor("jrperes","http://pixabay.com/de/mond-himmel-wolken-tag-v%C3%B6gel-323425/", "CC0 Public Domain","moon", null);easyBuild(context, author, newImages,R.drawable.moon, "MOND", "MOON");
+author = new ImageAuthor("catherinemary","http://pixabay.com/de/bank-rot-himmel-blau-natur-185234/", "CC0 Public Domain","bench", null);easyBuild(context, author, newImages,R.drawable.bench, "BANK", "BENCH");
+author = new ImageAuthor("MJO","http://pixabay.com/de/rosa-rot-blume-geschenk-143445/", "CC0 Public Domain","pink", null);easyBuild(context, author, newImages,R.drawable.pink, "ROSE", "ROSE");
+author = new ImageAuthor("rolypolys","http://pixabay.com/de/d%C3%A4nemark-ostsee-k%C3%BCste-meer-wasser-239992/", "CC0 Public Domain","denmark", null);easyBuild(context, author, newImages,R.drawable.denmark, "MEER", "SEA");
+author = new ImageAuthor("PublicDomainPictures","http://pixabay.com/de/hintergrund-blau-sauber-klar-tag-21717/", "CC0 Public Domain","background1", null);easyBuild(context, author, newImages,R.drawable.background1, "WIESE", "GRASSLAND?");
+author = new ImageAuthor("PublicDomainPictures","http://pixabay.com/de/hintergrund-nahaufnahme-flora-16051/", "CC0 Public Domain","background2", null);easyBuild(context, author, newImages,R.drawable.background2, "GRAS", "GRASS");
+author = new ImageAuthor("Maddox74","http://pixabay.com/de/nashorn-safaripark-d%C3%A4nemark-tier-433495/", "CC0 Public Domain","rhino4", null);easyBuild(context, author, newImages,R.drawable.rhino4, "NASHORN", "RHINO");
+author = new ImageAuthor("Kaz","http://pixabay.com/de/nashorn-tier-schwarz-silhouette-220220/", "CC0 Public Domain","rhino3", null);easyBuild(context, author, newImages,R.drawable.rhino3, "NASHORN", "RHINO");
+author = new ImageAuthor("OpenClips","http://pixabay.com/de/nashorn-afrika-tier-s%C3%BCdafrika-161569/", "CC0 Public Domain","rhino2", null);easyBuild(context, author, newImages,R.drawable.rhino2, "NASHORN", "RHINO");
+author = new ImageAuthor("OpenClips","http://pixabay.com/de/nashorn-tier-biologie-s%C3%A4ugetier-153558/", "CC0 Public Domain","rhino1", null);easyBuild(context, author, newImages,R.drawable.rhino1, "NASHORN", "RHINO");
+author = new ImageAuthor("OpenClips","http://pixabay.com/de/drache-eidechse-monster-chinesisch-149393/", "CC0 Public Domain","dragon", null);easyBuild(context, author, newImages,R.drawable.dragon, "DRACHE", "DRAGON");
+author = new ImageAuthor("OpenClips","http://pixabay.com/de/stier-buffalo-tier-s%C3%A4ugetier-155411/", "CC0 Public Domain","bull", null);easyBuild(context, author, newImages,R.drawable.bull, "STIER", "BULL");
+author = new ImageAuthor("Hebi65","http://pixabay.com/de/fledermaus-schwarz-dracula-fl%C3%BCgel-151366/", "CC0 Public Domain","bat", null);easyBuild(context, author, newImages,R.drawable.bat, "FLEDERMAUS", "BAT");
+author = new ImageAuthor("Hebi65","http://pixabay.com/de/blume-rose-kontur-umrisse-schwarz-681009/", "CC0 Public Domain","flower", null);easyBuild(context, author, newImages,R.drawable.flower, "BLUME", "FLOWER");
+author = new ImageAuthor("Hebi65","http://pixabay.com/de/schmetterling-schwarz-weiss-konturen-658047/", "CC0 Public Domain","butterfly", null);easyBuild(context, author, newImages,R.drawable.butterfly, "SCHMETTERLING", "BUTTERFLY");
+author = new ImageAuthor("Hebi65","http://pixabay.com/de/tier-katze-kontur-umrisse-675646/", "CC0 Public Domain","?", null);easyBuild(context, author, newImages,R.drawable.?, "KATZE", "CAT");
+author = new ImageAuthor("Hebi65","http://pixabay.com/de/baum-scherenschnitt-natur-657481/", "CC0 Public Domain","tree", null);easyBuild(context, author, newImages,R.drawable.tree, "BAUM", "TREE");
+author = new ImageAuthor("OpenClips","http://pixabay.com/de/karte-umrissen-spielen-schwarz-157404/", "CC0 Public Domain","card", null);easyBuild(context, author, newImages,R.drawable.card, "KARTE/PIK", "CARD/PEAK");
+author = new ImageAuthor("traude","http://pixabay.com/de/telefon-handy-telefonieren-558022/", "CC0 Public Domain","phone", null);easyBuild(context, author, newImages,R.drawable.phone, "TELEFON", "PHONE");
+author = new ImageAuthor("Nemo","http://pixabay.com/de/flasche-ketchup-leere-geschlossen-306549/", "CC0 Public Domain","bottle", null);easyBuild(context, author, newImages,R.drawable.bottle, "FLASCH", "BOTTLE");
+author = new ImageAuthor("Nemo","http://pixabay.com/de/elefant-profil-rot-gro%C3%9F-306223/", "CC0 Public Domain","elephant", null);easyBuild(context, author, newImages,R.drawable.elephant, "ELEFANT", "ELEPHANT");
+author = new ImageAuthor("Nemo","http://pixabay.com/de/brillen-schwarz-silhouette-310516/", "CC0 Public Domain","eyeglasses", null);easyBuild(context, author, newImages,R.drawable.eyeglasses, "BRILLE", "GLASSES");
+author = new ImageAuthor("Nemo","http://pixabay.com/de/k%C3%A4nguru-s%C3%A4ugetier-australien-295261/", "CC0 Public Domain","kangaroo", null);easyBuild(context, author, newImages,R.drawable.kangaroo, "KÄNGURU", "KANGAROO");
+author = new ImageAuthor("Nemo","http://pixabay.com/de/amsel-stehen-silhouette-rosa-305542/", "CC0 Public Domain","blackbird", null);easyBuild(context, author, newImages,R.drawable.blackbird, "AMSEL", "BLACKBIRD");
+author = new ImageAuthor("Nemo","http://pixabay.com/de/au%C3%9Ferirdischer-geste-des-friedens-308429/", "CC0 Public Domain","alien", null);easyBuild(context, author, newImages,R.drawable.alien, "ALIEN", "ALIEN");
+author = new ImageAuthor("Nemo","http://pixabay.com/de/teddyb%C3%A4r-b%C3%A4r-pl%C3%BCsch-gef%C3%BCllt-anial-303837/", "CC0 Public Domain","teddy-bear", null);easyBuild(context, author, newImages,R.drawable.teddy-bear, "TEDDYBÄR", "TEDDY");
+author = new ImageAuthor("Nemo","http://pixabay.com/de/fox-blau-silhouette-kunst-tierwelt-310123/", "CC0 Public Domain","fox", null);easyBuild(context, author, newImages,R.drawable.fox, "FUCHS", "FOX");
+author = new ImageAuthor("Nemo","http://pixabay.com/de/h%C3%A4nde-zwei-offen-silhouette-296850/", "CC0 Public Domain","hands", null);easyBuild(context, author, newImages,R.drawable.hands, "HAND/HÄNDE", "HANDS");
+author = new ImageAuthor("Nemo","http://pixabay.com/de/monitor-flatscreen-bildschirm-23269/", "CC0 Public Domain","monitor", null);easyBuild(context, author, newImages,R.drawable.monitor, "BILDSCHIRM", "MONITOR");
+author = new ImageAuthor("KTEditor","http://pixabay.com/de/uhr-zeit-stunden-559963/", "CC0 Public Domain","clock", null);easyBuild(context, author, newImages,R.drawable.clock, "UHR", "CLOCK");
+author = new ImageAuthor("amandaelizabeth84","http://pixabay.com/de/schnurrbart-lenker-m%C3%A4nnlich-haar-473661/", "CC0 Public Domain","moustache", null);easyBuild(context, author, newImages,R.drawable.moustache, "SCHNURRBART", "MOUSTACHE");
 
     }
 
